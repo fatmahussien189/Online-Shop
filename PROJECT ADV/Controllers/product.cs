af8cd8e15d490207851ff1ea9e33f0981696359f
fatmaeladawy <fatmahussiensayed150@commerce.helwan.edu.eg>
2018-12-11 01:47:03 +0200

    .

---
+++ PROJECT ADV/Controllers/product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PROJECT.ADV.Controllers
+{
+    public class product
+{
+}
+}
---
+++ PROJECT ADV/Models/shoes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PROJECT.ADV.Models
+{
+    public class shoes:product
+
+{
+        public int size { get; set; }
+        public string color { get; set; }
+        public string material { get; set; }
+        public string Brand { get; set; }
+    }
+}
